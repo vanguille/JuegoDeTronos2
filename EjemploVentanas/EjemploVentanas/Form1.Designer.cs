4ecaf54239f613fa3a9dfcbce5dd13c8c884e452
Javier Fdez-Trapa <javier.ftrapa@gmail.com>
2015-04-24 10:29:21 +0200

    prueba

--- EjemploVentanas/EjemploVentanas/Form1.Designer.cs
+++ EjemploVentanas/EjemploVentanas/Form1.Designer.cs
@@ -47,7 +47,7 @@
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button1.Location = new System.Drawing.Point(71, 265);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 98);
+            this.button1.Size = new System.Drawing.Size(125, 98);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
